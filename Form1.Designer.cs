09bee97706b6c9de105c8b5db3703c378783dd96
Troybert <59945550+oddjob775@users.noreply.github.com>
2020-07-13 16:03:03 -0700

    Initial Submission

---
+++ Form1.Designer.cs
@@ -0,0 +1,360 @@
+﻿namespace Calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.divideBtn = new System.Windows.Forms.Button();
+            this.multBtn = new System.Windows.Forms.Button();
+            this.subBtn = new System.Windows.Forms.Button();
+            this.addBtn = new System.Windows.Forms.Button();
+            this.decBtn = new System.Windows.Forms.Button();
+            this.equalBtn = new System.Windows.Forms.Button();
+            this.btn0 = new System.Windows.Forms.Button();
+            this.resultBox = new System.Windows.Forms.TextBox();
+            this.clrBtn = new System.Windows.Forms.Button();
+            this.brandbox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btn7
+            // 
+            this.btn7.BackColor = System.Drawing.Color.Black;
+            this.btn7.Font = new System.Drawing.Font("Arimo", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn7.ForeColor = System.Drawing.Color.White;
+            this.btn7.Location = new System.Drawing.Point(12, 154);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(66, 50);
+            this.btn7.TabIndex = 0;
+            this.btn7.Text = "7";
+            this.btn7.UseVisualStyleBackColor = false;
+            this.btn7.Click += new System.EventHandler(this.click_button);
+            // 
+            // btn9
+            // 
+            this.btn9.BackColor = System.Drawing.Color.Black;
+            this.btn9.Font = new System.Drawing.Font("Arimo", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn9.ForeColor = System.Drawing.Color.White;
+            this.btn9.Location = new System.Drawing.Point(156, 154);
+            this.btn9.Name = "btn9";
+            this.btn9.Size = new System.Drawing.Size(66, 50);
+            this.btn9.TabIndex = 1;
+            this.btn9.Text = "9";
+            this.btn9.UseVisualStyleBackColor = false;
+            this.btn9.Click += new System.EventHandler(this.click_button);
+            // 
+            // btn8
+            // 
+            this.btn8.BackColor = System.Drawing.Color.Black;
+            this.btn8.Font = new System.Drawing.Font("Arimo", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn8.ForeColor = System.Drawing.Color.White;
+            this.btn8.Location = new System.Drawing.Point(84, 154);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(66, 50);
+            this.btn8.TabIndex = 2;
+            this.btn8.Text = "8";
+            this.btn8.UseVisualStyleBackColor = false;
+            this.btn8.Click += new System.EventHandler(this.click_button);
+            // 
+            // btn5
+            // 
+            this.btn5.BackColor = System.Drawing.Color.Black;
+            this.btn5.Font = new System.Drawing.Font("Arimo", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn5.ForeColor = System.Drawing.Color.White;
+            this.btn5.Location = new System.Drawing.Point(84, 210);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(66, 50);
+            this.btn5.TabIndex = 5;
+            this.btn5.Text = "5";
+            this.btn5.UseVisualStyleBackColor = false;
+            this.btn5.Click += new System.EventHandler(this.click_button);
+            // 
+            // btn6
+            // 
+            this.btn6.BackColor = System.Drawing.Color.Black;
+            this.btn6.Font = new System.Drawing.Font("Arimo", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn6.ForeColor = System.Drawing.Color.White;
+            this.btn6.Location = new System.Drawing.Point(156, 210);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(66, 50);
+            this.btn6.TabIndex = 4;
+            this.btn6.Text = "6";
+            this.btn6.UseVisualStyleBackColor = false;
+            this.btn6.Click += new System.EventHandler(this.click_button);
+            // 
+            // btn4
+            // 
+            this.btn4.BackColor = System.Drawing.Color.Black;
+            this.btn4.Font = new System.Drawing.Font("Arimo", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn4.ForeColor = System.Drawing.Color.White;
+            this.btn4.Location = new System.Drawing.Point(12, 210);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(66, 50);
+            this.btn4.TabIndex = 3;
+            this.btn4.Text = "4";
+            this.btn4.UseVisualStyleBackColor = false;
+            this.btn4.Click += new System.EventHandler(this.click_button);
+            // 
+            // btn2
+            // 
+            this.btn2.BackColor = System.Drawing.Color.Black;
+            this.btn2.Font = new System.Drawing.Font("Arimo", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn2.ForeColor = System.Drawing.Color.White;
+            this.btn2.Location = new System.Drawing.Point(84, 266);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(66, 50);
+            this.btn2.TabIndex = 8;
+            this.btn2.Text = "2";
+            this.btn2.UseVisualStyleBackColor = false;
+            this.btn2.Click += new System.EventHandler(this.click_button);
+            // 
+            // btn3
+            // 
+            this.btn3.BackColor = System.Drawing.Color.Black;
+            this.btn3.Font = new System.Drawing.Font("Arimo", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn3.ForeColor = System.Drawing.Color.White;
+            this.btn3.Location = new System.Drawing.Point(156, 266);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(66, 50);
+            this.btn3.TabIndex = 7;
+            this.btn3.Text = "3";
+            this.btn3.UseVisualStyleBackColor = false;
+            this.btn3.Click += new System.EventHandler(this.click_button);
+            // 
+            // btn1
+            // 
+            this.btn1.BackColor = System.Drawing.Color.Black;
+            this.btn1.Font = new System.Drawing.Font("Arimo", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn1.ForeColor = System.Drawing.Color.White;
+            this.btn1.Location = new System.Drawing.Point(12, 266);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(66, 50);
+            this.btn1.TabIndex = 6;
+            this.btn1.Text = "1";
+            this.btn1.UseVisualStyleBackColor = false;
+            this.btn1.Click += new System.EventHandler(this.click_button);
+            // 
+            // divideBtn
+            // 
+            this.divideBtn.BackColor = System.Drawing.Color.Black;
+            this.divideBtn.Font = new System.Drawing.Font("Arimo", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.divideBtn.ForeColor = System.Drawing.Color.White;
+            this.divideBtn.Location = new System.Drawing.Point(228, 154);
+            this.divideBtn.Name = "divideBtn";
+            this.divideBtn.Size = new System.Drawing.Size(66, 50);
+            this.divideBtn.TabIndex = 9;
+            this.divideBtn.Text = "÷";
+            this.divideBtn.UseVisualStyleBackColor = false;
+            this.divideBtn.Click += new System.EventHandler(this.operator_click);
+            // 
+            // multBtn
+            // 
+            this.multBtn.BackColor = System.Drawing.Color.Black;
+            this.multBtn.Font = new System.Drawing.Font("Arimo", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.multBtn.ForeColor = System.Drawing.Color.White;
+            this.multBtn.Location = new System.Drawing.Point(228, 210);
+            this.multBtn.Name = "multBtn";
+            this.multBtn.Size = new System.Drawing.Size(66, 50);
+            this.multBtn.TabIndex = 10;
+            this.multBtn.Text = "×";
+            this.multBtn.UseVisualStyleBackColor = false;
+            this.multBtn.Click += new System.EventHandler(this.operator_click);
+            // 
+            // subBtn
+            // 
+            this.subBtn.BackColor = System.Drawing.Color.Black;
+            this.subBtn.Font = new System.Drawing.Font("Arimo", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.subBtn.ForeColor = System.Drawing.Color.White;
+            this.subBtn.Location = new System.Drawing.Point(228, 266);
+            this.subBtn.Name = "subBtn";
+            this.subBtn.Size = new System.Drawing.Size(66, 50);
+            this.subBtn.TabIndex = 11;
+            this.subBtn.Text = "-";
+            this.subBtn.UseVisualStyleBackColor = false;
+            this.subBtn.Click += new System.EventHandler(this.operator_click);
+            // 
+            // addBtn
+            // 
+            this.addBtn.BackColor = System.Drawing.Color.Black;
+            this.addBtn.Font = new System.Drawing.Font("Arimo", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.addBtn.ForeColor = System.Drawing.Color.White;
+            this.addBtn.Location = new System.Drawing.Point(228, 322);
+            this.addBtn.Name = "addBtn";
+            this.addBtn.Size = new System.Drawing.Size(66, 50);
+            this.addBtn.TabIndex = 12;
+            this.addBtn.Text = "+";
+            this.addBtn.UseVisualStyleBackColor = false;
+            this.addBtn.Click += new System.EventHandler(this.operator_click);
+            // 
+            // decBtn
+            // 
+            this.decBtn.BackColor = System.Drawing.Color.Black;
+            this.decBtn.Font = new System.Drawing.Font("Arimo", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.decBtn.ForeColor = System.Drawing.Color.White;
+            this.decBtn.Location = new System.Drawing.Point(84, 322);
+            this.decBtn.Name = "decBtn";
+            this.decBtn.Size = new System.Drawing.Size(66, 50);
+            this.decBtn.TabIndex = 15;
+            this.decBtn.Text = ".";
+            this.decBtn.UseVisualStyleBackColor = false;
+            this.decBtn.Click += new System.EventHandler(this.click_button);
+            // 
+            // equalBtn
+            // 
+            this.equalBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.equalBtn.Font = new System.Drawing.Font("Arimo", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.equalBtn.ForeColor = System.Drawing.Color.White;
+            this.equalBtn.Location = new System.Drawing.Point(156, 322);
+            this.equalBtn.Name = "equalBtn";
+            this.equalBtn.Size = new System.Drawing.Size(66, 50);
+            this.equalBtn.TabIndex = 14;
+            this.equalBtn.Text = "=";
+            this.equalBtn.UseVisualStyleBackColor = false;
+            this.equalBtn.Click += new System.EventHandler(this.equalBtn_Click);
+            // 
+            // btn0
+            // 
+            this.btn0.BackColor = System.Drawing.Color.Black;
+            this.btn0.Font = new System.Drawing.Font("Arimo", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn0.ForeColor = System.Drawing.Color.White;
+            this.btn0.Location = new System.Drawing.Point(12, 322);
+            this.btn0.Name = "btn0";
+            this.btn0.Size = new System.Drawing.Size(66, 50);
+            this.btn0.TabIndex = 13;
+            this.btn0.Text = "0";
+            this.btn0.UseVisualStyleBackColor = false;
+            this.btn0.Click += new System.EventHandler(this.click_button);
+            // 
+            // resultBox
+            // 
+            this.resultBox.BackColor = System.Drawing.Color.LightGray;
+            this.resultBox.Font = new System.Drawing.Font("Arimo", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resultBox.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.resultBox.Location = new System.Drawing.Point(12, 12);
+            this.resultBox.Multiline = true;
+            this.resultBox.Name = "resultBox";
+            this.resultBox.Size = new System.Drawing.Size(282, 68);
+            this.resultBox.TabIndex = 16;
+            this.resultBox.Text = "0";
+            this.resultBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // clrBtn
+            // 
+            this.clrBtn.BackColor = System.Drawing.Color.Maroon;
+            this.clrBtn.Font = new System.Drawing.Font("Arimo", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clrBtn.ForeColor = System.Drawing.Color.White;
+            this.clrBtn.Location = new System.Drawing.Point(12, 98);
+            this.clrBtn.Name = "clrBtn";
+            this.clrBtn.Size = new System.Drawing.Size(66, 50);
+            this.clrBtn.TabIndex = 17;
+            this.clrBtn.Text = "C";
+            this.clrBtn.UseVisualStyleBackColor = false;
+            this.clrBtn.Click += new System.EventHandler(this.clrBtn_Click);
+            // 
+            // brandbox
+            // 
+            this.brandbox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.brandbox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.brandbox.Font = new System.Drawing.Font("Arimo", 22.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.brandbox.ForeColor = System.Drawing.Color.DarkGray;
+            this.brandbox.Location = new System.Drawing.Point(84, 98);
+            this.brandbox.Multiline = true;
+            this.brandbox.Name = "brandbox";
+            this.brandbox.ReadOnly = true;
+            this.brandbox.Size = new System.Drawing.Size(210, 50);
+            this.brandbox.TabIndex = 18;
+            this.brandbox.Text = "NVTECH";
+            this.brandbox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(305, 383);
+            this.Controls.Add(this.brandbox);
+            this.Controls.Add(this.clrBtn);
+            this.Controls.Add(this.resultBox);
+            this.Controls.Add(this.decBtn);
+            this.Controls.Add(this.equalBtn);
+            this.Controls.Add(this.btn0);
+            this.Controls.Add(this.addBtn);
+            this.Controls.Add(this.subBtn);
+            this.Controls.Add(this.multBtn);
+            this.Controls.Add(this.divideBtn);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.btn5);
+            this.Controls.Add(this.btn6);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.btn8);
+            this.Controls.Add(this.btn9);
+            this.Controls.Add(this.btn7);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btn9;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button divideBtn;
+        private System.Windows.Forms.Button multBtn;
+        private System.Windows.Forms.Button subBtn;
+        private System.Windows.Forms.Button addBtn;
+        private System.Windows.Forms.Button decBtn;
+        private System.Windows.Forms.Button equalBtn;
+        private System.Windows.Forms.Button btn0;
+        private System.Windows.Forms.TextBox resultBox;
+        private System.Windows.Forms.Button clrBtn;
+        private System.Windows.Forms.TextBox brandbox;
+    }
+}
+
